c3c6c93dd12bb3b66f5df72da58679b628ad38a7
Storm <dpkstav@gmail.com>
2025-04-01 00:27:04 +0300

    count students

---
+++ Academy/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Academy
+{
+    public partial class MainForm : Form
+    {
+        Connector connector;
+        public MainForm()
+        {
+            InitializeComponent();
+            connector = new Connector(ConfigurationManager.ConnectionStrings ["VPD_311_Import"].ConnectionString);
+            dgvStudents.DataSource = connector.Select("*","Students");
+            statusStripCountLabel.Text = $"Колличество студетов: {dgvStudents.RowCount-1}";
+        }
+    }
+}
--- MoviesForms/TableForm.Designer.cs
+++ MoviesForms/TableForm.Designer.cs
@@ -44,13 +44,13 @@ namespace MoviesForms
             this.dataGridView1.Size = new System.Drawing.Size(800, 450);
             this.dataGridView1.TabIndex = 0;
             // 
-            // Base
+            // TableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "Base";
+            this.Name = "TableForm";
             this.Text = "Table";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
